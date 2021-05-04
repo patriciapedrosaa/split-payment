8cb45991f265ba125fcce0874e5852c8c51811e7
Patrícia Pedrosa <patricia.pedrosaa@gmail.com>
2021-05-03 23:59:01 -0300

    feat: Adds main and go mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module split-payment
+
+go 1.16
+
+require github.com/stretchr/testify v1.7.0
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"split-payment/service"
+)
+
+const(
+	PATH_SHOPPING_LIST_FILE = "resources/shoppingList.json"
+	PATH_EMAILS_LIST_FILE = "resources/emails.json"
+)
+
+var (
+	readService = service.NewReadService(service.NewConvertService())
+	calculatorService = service.NewCalculatorService(service.NewConvertService())
+)
+
+func main() {
+	shoppingList, err := readService.ReadShoppingListFile(PATH_SHOPPING_LIST_FILE)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	emailsList, err := readService.ReadEmailsListFile(PATH_EMAILS_LIST_FILE)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	splitPayment, err := calculatorService.CalcPaymentByEmail(shoppingList, emailsList)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(splitPayment)
+}
+
